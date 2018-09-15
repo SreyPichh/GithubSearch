c56fea0d7b8b09b57b4cb8e78b3aa97f14b6ee45
SreyPichh <phansreypich2015@gmail.com>
2018-09-15 11:38:07 +0700

    adding utils class with apikit extension

---
+++ SearchThenLib/Classes/Utils/Extensions/UIView+extensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIView+extensions.swift
+//  SearchThenLib
+//
+//  Created by ken.phanith on 2018/09/12.
+//  Copyright © 2018 Pich. All rights reserved.
+//
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    var safeArea: 
+    
+}
